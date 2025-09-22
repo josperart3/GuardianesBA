c197c81a75b58ded4fff5910241ad8b6d4b56cb8
josperart3 <josperart3@alum.us.es>
2025-09-22 13:29:58 +0200

    Actualizado spring boot

--- guardianes-service/src/main/java/us/dit/service/Application.java
+++ guardianes-service/src/main/java/us/dit/service/Application.java
@@ -2,12 +2,15 @@ package us.dit.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "us.dit.service.model.entities")
+@EnableJpaRepositories("us.dit.service.model.repositories")
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
